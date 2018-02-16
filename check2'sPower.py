3d95f9081ff274116f27b647fcd022b94450a5f7
vijay sawant <vijaysawant36@gmail.com>
2018-02-16 18:46:34 +0530

    update one file
    this program is for to check given num is power of 2's or not.

--- check2'sPower.py
+++ check2'sPower.py
@@ -1,26 +1,32 @@
-
-def check2sPower(Num):
-	m = 1
+"""
+Check given number is power of 2's or not.
+ex - 1,2,4,8,16,32 are power of 2's
+"""
+def Check2sPower(Num):
+	if Num == 0:
+		return 0
 	cnt = 0
-	while m < Num:
-		if Num & m == 1:
+	m = 1
+	while m <= Num:
+		if Num & m == m:
 			cnt += 1
 		m = m << 1
-			
+	print "Num of 1's : ",cnt
+	if cnt == 1:
+		return 1
+	else:
+		return 0
+		
+"""
+or simple solution
+"""
+def Check2sPower_or_not(num):
+	return num != 0 and (num & (num-1) == 0)
+		
 
 if __name__ == "__main__":
-	num = input("Enter num : ")
-	if (check2sPower(num)):
-		print "True"
+	Num = input("Enter Number : ")
+	if Check2sPower_or_not(Num):
+		print "Yes {0} is power of 2's".format(Num)
 	else:
-		print"False"
-'''def Generic2sPowerDivisibilityTest(number,divisor):
-	return (number & (divisor - 1))==0
-	
-if __name__ == "__main__":
-	num, div = input("Enter num and divisor comma seperatly : ")
-	if Generic2sPowerDivisibilityTest(num,div):
-		print "True number is divisible divisor"
-	else:
-		print "False number is not divisible divisor"
-'''
+		print "No {0} is not power of 2's".format(Num)
